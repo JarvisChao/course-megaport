aba4dda7f903c345f22922a4fd54145fe36e07b8
Jarvis Chao <jarvis.pro73045@gmail.com>
2024-03-26 00:52:15 +0800

    refactor: 修改 meta 佈局及 icon 等等

--- src/js/main.js
+++ src/js/main.js
@@ -82,21 +82,21 @@ window.addEventListener('scroll', function() {
 });
 
 // Go Top
-function gotop(y, duration = 1000) {
-  const initialY = document.documentElement.scrollTop || document.body.scrollTop;
-  const baseY = (initialY + y) * 0.5;
-  const difference = initialY - baseY;
-  const startTime = performance.now();
-  const step = () => {
-    let normalizedTime = (performance.now() - startTime) / duration;
-    if (normalizedTime > 1) {
-      normalizedTime = 1;
-    }
-    window.scrollTo(0, baseY + difference * Math.cos(normalizedTime * Math.PI));
-    if (normalizedTime < 1) window.requestAnimationFrame(step);
-  };
-  window.requestAnimationFrame(step);
-}
+// function gotop(y, duration = 1000) {
+//   const initialY = document.documentElement.scrollTop || document.body.scrollTop;
+//   const baseY = (initialY + y) * 0.5;
+//   const difference = initialY - baseY;
+//   const startTime = performance.now();
+//   const step = () => {
+//     let normalizedTime = (performance.now() - startTime) / duration;
+//     if (normalizedTime > 1) {
+//       normalizedTime = 1;
+//     }
+//     window.scrollTo(0, baseY + difference * Math.cos(normalizedTime * Math.PI));
+//     if (normalizedTime < 1) window.requestAnimationFrame(step);
+//   };
+//   window.requestAnimationFrame(step);
+// }
 
 /* ----------------------------------- */
 /* ------ Plugin ------ */
